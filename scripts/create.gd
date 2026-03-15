ae251a464613e9630404601aa819f8ba20ce3913
Cahya Putra <cahyaputrapratama52@gmail.com>
2026-03-15 09:17:14 +0700

    add push pull feature

---
+++ scripts/create.gd
@@ -0,0 +1,8 @@
+extends RigidBody2D
+
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,12 @@ extends CharacterBody2D
 
 @export var speed = 300.0
 var jump_velocity = -240.0
+@export var push_force = 200
+@export var pull_force = 150
 
 @onready var animation: AnimatedSprite2D = $AnimatedSprite2D
 @onready var camera: Camera2D = $Camera2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -52,5 +55,22 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+	
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		
+		if collision.get_collider() is RigidBody2D:
+			var body = collision.get_collider()
+			body.apply_central_impulse(Vector2(direction * push_force, 0))
+	
+	if Input.is_action_pressed("pull"):
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+
+			if collision.get_collider() is RigidBody2D:
+				var body = collision.get_collider()
+				
+				var pull_dir = global_position.direction_to(body.global_position)
+				body.apply_central_impulse(-pull_dir * pull_force)
