f67eb9902987bcbc1fff4a57ccb2e805594bef1d
Cahya Putra <cahyaputrapratama52@gmail.com>
2026-05-30 21:15:16 +0700

    repair week 8

--- week8/transition_manager.gd
+++ week8/transition_manager.gd
@@ -2,14 +2,23 @@ extends CanvasLayer
 
 @onready var color_rect: ColorRect = $ColorRect
 
-@export var transition_duration: float = 0.5
+@export var transition_duration: float = 0.4
+
+var shader_material: ShaderMaterial
+
+func _ready() -> void:
+	shader_material = color_rect.material as ShaderMaterial
+	shader_material.set_shader_parameter("radius", 1.0)
+	color_rect.visible = false
 
 func change_scene(target_scene_path: String) -> void:
+	color_rect.visible = true
+	
 	var tween_in := create_tween()
 	
-	tween_in.tween_property(color_rect, "modulate:a", 1.0, transition_duration)\
-		.set_trans(Tween.TRANS_SINE)\
-		.set_ease(Tween.EASE_IN_OUT)
+	tween_in.tween_property(shader_material, "shader_parameter/radius", 0.0, transition_duration)\
+		.set_trans(Tween.TRANS_QUINT)\
+		.set_ease(Tween.EASE_OUT)
 	
 	await tween_in.finished
 	
@@ -19,8 +28,10 @@ func change_scene(target_scene_path: String) -> void:
 	
 	var tween_out := create_tween()
 	
-	tween_out.tween_property(color_rect, "modulate:a", 0.0, transition_duration)\
-		.set_trans(Tween.TRANS_SINE)\
-		.set_ease(Tween.EASE_IN_OUT)
+	tween_out.tween_property(shader_material, "shader_parameter/radius", 1.0, transition_duration)\
+		.set_trans(Tween.TRANS_QUINT)\
+		.set_ease(Tween.EASE_IN)
 	
 	await tween_out.finished
+	
+	color_rect.visible = false
